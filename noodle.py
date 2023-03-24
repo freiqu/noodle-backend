62648c37ae5ce8b5e9f9a866ae1cf751a970aba1
Varvara <zolotykh2020@gmail.com>
2023-03-24 09:54:30 +0100

    insert

--- noodle.py
+++ noodle.py
@@ -1,49 +1,61 @@
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, request, render_template, json
 from flask_cors import CORS
 import sqlite3
 import datetime
 import pytz
+import secretstorage
+
 
 current_time = datetime.datetime.now(pytz.timezone('Europe/Berlin'))
 
 app = Flask(__name__)
 CORS(app)
+user = "Noodle"
+curID = 0
 
 connect = sqlite3.connect('comments.db')
 connect.execute(
-	'CREATE TABLE IF NOT EXISTS COMMENTS (ID INTEGER, content TEXT, user TEXT, time TEXT)')
+    'CREATE TABLE IF NOT EXISTS COMMENTS (ID INTEGER, content TEXT, user TEXT, time TEXT)')
 
-def insert(ID, content, user, time):
-      connect = sqlite3.connect('comments.db')
-      connect.execute("INSERT INTO COMMENTS (ID,content,user,time) VALUES (?,?,?)", (ID, content, user, time))
 
-@app.route('/join', methods=['GET', 'POST'])
-def join():
-	if request.method == 'POST':
-		firstname = request.form['firstname']
-		name = request.form['name']
-		email = request.form['email']
-		city = request.form['city']
-		country = request.form['country']
-		phone = request.form['phone']
+def insert(content, user, time=datetime.datetime.now(pytz.timezone('Europe/Berlin'))):
+    global curID
+    connect = sqlite3.connect('comments.db')
+    connect.execute("INSERT INTO COMMENTS (ID,content,user,time) VALUES (?,?,?,?)",
+                    (curID, content, user, time))
+    curID += 1
 
-		with sqlite3.connect("database.db") as users:
-			cursor = users.cursor()
-			cursor.execute("INSERT INTO PARTICIPANTS (firstname,name,email,city,country,phone) VALUES (?,?,?,?,?,?)", (firstname, name, email, city, country, phone))
-			users.commit()
-		return render_template("index.html")
-	else:
-		return render_template('join.html')
 
-@app.route('/hello', methods=['GET'])
-def getScore():
-    return Response("Hello, World", status=200, mimetype="text/plain")
+def read():
+    connect = sqlite3.connect('comments.db')
+    posts = connect.execute('SELECT * FROM COMMENTS;')
 
-@app.route('/news')
-def participants():
-	connect = sqlite3.connect('comments.db')
-	cursor = connect.cursor()
-	cursor.execute('SELECT * FROM COMMENTS')
-	data = cursor.fetchall()
-	return render_template("news.html", data=data)
 
+@app.route('/')
+def index():
+    return Response('index.html', status=200, mimetype="text/plain")
+
+
+@app.route('/news', methods=['GET'])
+def news():
+    if request.method == 'GET':
+        connect = sqlite3.connect('comments.db')
+        cursor = connect.cursor()
+        cursor.execute('SELECT * FROM COMMENTS')
+
+        data = cursor.fetchall()
+        comments = {}
+        for i in data:
+            pass
+        return Response(status=200, mimetype="text/plain")
+
+
+"""
+{
+    "posts": {
+        "content": "hab heute dies un das gemacht"
+        "user": "Reiner Zufall"
+        "time": "2023-03-23 14:21:34"
+    }
+}
+"""
--- noodle.wsgi
+++ noodle.wsgi
@@ -1,6 +1,4 @@
 #!/bin/python3
-import sys
-sys.path.insert(0, '/home/dev/crawled/Website')
 from noodle import app as application
 
 if __name__ == '__main__':
