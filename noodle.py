c9307cb95dadb2d57f4abcbabd0c4b9955d654ed
Varvara <zolotykh2020@gmail.com>
2023-03-24 09:54:13 +0100

    insert

--- noodle.py
+++ noodle.py
@@ -13,10 +13,27 @@ connect = sqlite3.connect('comments.db')
 connect.execute(
 	'CREATE TABLE IF NOT EXISTS COMMENTS (ID INTEGER, content TEXT, user TEXT, time TEXT)')
 
-def insert():
+def insert(ID, content, user, time):
       connect = sqlite3.connect('comments.db')
-      connect.execute('''INSERT INTO COMMENTS ()
-      ''')
+      connect.execute("INSERT INTO COMMENTS (ID,content,user,time) VALUES (?,?,?)", (ID, content, user, time))
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+	if request.method == 'POST':
+		firstname = request.form['firstname']
+		name = request.form['name']
+		email = request.form['email']
+		city = request.form['city']
+		country = request.form['country']
+		phone = request.form['phone']
+
+		with sqlite3.connect("database.db") as users:
+			cursor = users.cursor()
+			cursor.execute("INSERT INTO PARTICIPANTS (firstname,name,email,city,country,phone) VALUES (?,?,?,?,?,?)", (firstname, name, email, city, country, phone))
+			users.commit()
+		return render_template("index.html")
+	else:
+		return render_template('join.html')
 
 @app.route('/hello', methods=['GET'])
 def getScore():
@@ -29,3 +46,4 @@ def participants():
 	cursor.execute('SELECT * FROM COMMENTS')
 	data = cursor.fetchall()
 	return render_template("news.html", data=data)
+
